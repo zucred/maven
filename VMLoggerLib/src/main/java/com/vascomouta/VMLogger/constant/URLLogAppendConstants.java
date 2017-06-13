d3a149d1b2364d7dc2160a490e4216fa36f055f1
Sourabh Kapoor <kapoor.utd@gmail.com>
2017-06-13 19:05:45 +0530

    capture app lifecycle event and code optimization

---
+++ VMLoggerLib/src/main/java/com/vascomouta/VMLogger/constant/URLLogAppendConstants.java
@@ -0,0 +1,9 @@
+package com.vascomouta.VMLogger.constant;
+
+public class URLLogAppendContants {
+
+    public static final String ServerUrl = "url";
+    public static final String Headers = "headers";
+    public static final String Method = "method";
+    public static final String Parameter = "parameter";
+}
---
+++ app/src/main/java/com/vascomouta/VMLogger_example/ApplicationLifecycleHandler.java
@@ -0,0 +1,8 @@
+package com.vascomouta.VMLogger_example;
+
+/**
+ * Created by Asma on 13/06/17.
+ */
+
+public class ApplicationLifecycleHandler {
+}
